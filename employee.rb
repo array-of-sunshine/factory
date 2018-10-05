8fe5a90b18a6da0712a8a53b6422a13fb7d7dd34
Brian <riceb53@gmail.com>
2018-10-04 19:45:28 -0500

    add options hash

--- employee.rb
+++ employee.rb
@@ -12,7 +12,7 @@
 # p "#{employee2[0]} #{employee2[1]} makes $#{employee2[2]} per year."
 
 # hash
-employee1 = {:first_name => "Danilo", :last_name => "Campos", :salary => 80000, :active => true}
+# employee1 = { :last_name => "Campos", :first_name => "Danilo", :salary => 80000, :active => true}
 # employee2 = {first_name: "Marjora", last_name: "Carter", salary: 70000, active: true}
 # employee1[:first_name] = "Brian"
 # p employee1
@@ -25,33 +25,14 @@ employee1 = {:first_name => "Danilo", :last_name => "Campos", :salary => 80000, 
 # class
 
 class Employee
-  def initialize(input_first_name, input_last_name, input_salary, input_active)
-    @first_name = input_first_name
-    @last_name = input_last_name
-    @salary = input_salary
-    @active = input_active
-  end
+  attr_reader :active, :first_name, :last_name, :salary
+  attr_writer :active
 
-  def first_name
-    @first_name
-  end
-
-  def last_name
-    @last_name
-  end
-
-  def salary
-    @salary
-  end
-
-  # reader
-  def active
-    @active
-  end
-
-  # writer
-  def active=(input_active)
-    @active = input_active
+  def initialize(input_options)
+    @first_name = input_options[:first_name]
+    @last_name = input_options[:last_name]
+    @salary = input_options[:salary]
+    @active = input_options[:active]
   end
 
   def print_info
@@ -63,22 +44,25 @@ class Employee
   end
 end
 
-employee1 = Employee.new("Danilo", "Campos", 80000, true)
-employee2 = Employee.new("Majora", "Carter", 70000, true)
+employee1 = Employee.new({:salary => 80000, :last_name => "Campos", :first_name => "Danilo", :active => true})
+
+employee2 = Employee.new(salary: 70000, last_name: "Carter", first_name: "Majora", active: true)
+
+# employee2 = Employee.new("Majora", "Carter", 70000, true)
 
 employee1.active = false
 p employee1.active
 # p 'employee 1 before raise'
 employee1.print_info
-# employee1.give_annual_raise
-# p 'employee1 after raise'
-# employee1.print_info
-# p "*" * 50
-# p "employee2 before raise"
-# employee2.print_info
-# employee2.give_annual_raise
-# p "employee2 after raise"
-# employee2.print_info
+employee1.give_annual_raise
+p 'employee1 after raise'
+employee1.print_info
+p "*" * 50
+p "employee2 before raise"
+employee2.print_info
+employee2.give_annual_raise
+p "employee2 after raise"
+employee2.print_info
 
 
 # p employee1.salary
